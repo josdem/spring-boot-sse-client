b225f1c703eadc9b31fe984c03a077f7dcac9527
josdem <joseluis.delacruz@gmail.com>
2019-05-12 19:48:33 -0400

    Deleting unused code

--- src/main/java/com/jos/dem/springboot/sse/client/controller/ServerSentEventsConsumerController.java
+++ src/main/java/com/jos/dem/springboot/sse/client/controller/ServerSentEventsConsumerController.java
@@ -7,9 +7,6 @@ import reactor.core.publisher.Flux;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.codec.ServerSentEvent;
 
 import com.jos.dem.springboot.sse.client.service.ServerSentEventsConsumerService;
--- src/main/java/com/jos/dem/springboot/sse/client/service/impl/ServerSentEventsConsumerServiceImpl.java
+++ src/main/java/com/jos/dem/springboot/sse/client/service/impl/ServerSentEventsConsumerServiceImpl.java
@@ -5,8 +5,8 @@ import reactor.core.publisher.Flux;
 import org.springframework.stereotype.Service;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.jos.dem.springboot.sse.client.service.ServerSentEventsConsumerService;
 
