602d849ce746e495f01a4fda7ab9cee38868b62d
josdem <joseluis.delacruz@gmail.com>
2019-05-12 12:36:20 -0400

    Adding webclient as a bean

--- src/main/java/com/jos/dem/springboot/sse/client/ServerSentEventsClientApplication.java
+++ src/main/java/com/jos/dem/springboot/sse/client/ServerSentEventsClientApplication.java
@@ -1,13 +1,20 @@
 package com.jos.dem.springboot.sse.client;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class ServerSentEventsClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerSentEventsClientApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ServerSentEventsClientApplication.class, args);
+  }
+
+  @Bean
+  WebClient webClient() {
+    return WebClient.create("http://localhost:8080/");
+  }
 
 }
