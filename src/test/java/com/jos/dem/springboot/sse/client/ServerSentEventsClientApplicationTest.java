2c9f1824ca90a8771cdc688dd659684a9cbc6e7e
josdem <joseluis.delacruz@gmail.com>
2020-10-31 16:19:13 -0400

    Adding Lombok to the test case

--- src/test/java/com/jos/dem/springboot/sse/client/ServerSentEventsClientApplicationTest.java
+++ src/test/java/com/jos/dem/springboot/sse/client/ServerSentEventsClientApplicationTest.java
@@ -1,41 +1,36 @@
 package com.jos.dem.springboot.sse.client;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import java.util.Date;
-import java.time.LocalTime;
-
-import reactor.core.publisher.Flux;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.codec.ServerSentEvent;
 
 import com.jos.dem.springboot.sse.client.service.ServerSentEventsConsumerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.codec.ServerSentEvent;
+import reactor.core.publisher.Flux;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
-@RunWith(SpringRunner.class)
+@Slf4j
 @SpringBootTest
-public class ServerSentEventsClientApplicationTest {
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+class ServerSentEventsClientApplicationTest {
 
-  @Autowired
-  private ServerSentEventsConsumerService service;
+    private final ServerSentEventsConsumerService service;
 
-  private Logger log = LoggerFactory.getLogger(this.getClass());
+    @Test
+    @DisplayName("Consume Server Sent Event")
+    public void shouldConsumeServerSentEvents(TestInfo testInfo) throws Exception {
+        log.info("Running: Consume server sent events: {}", LocalDate.now());
 
-	@Test
-	public void shouldConsumeServerSentEvents() throws Exception {
-    log.info("Running: Consume server sent events: {}", new Date());
+        Flux<ServerSentEvent<String>> eventStream = service.consume();
 
-    Flux<ServerSentEvent<String>> eventStream = service.consume();
-
-    eventStream.subscribe(ctx ->
-        log.info("Current time: {}, content[{}] ", LocalTime.now(), ctx.data()));
-	}
+        eventStream.subscribe(ctx ->
+                log.info("Current time: {}, content[{}] ", LocalTime.now(), ctx.data()));
+    }
 
 }
